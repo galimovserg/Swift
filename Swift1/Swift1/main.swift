e67e9e2c9c4c90cdb348535d92e441b199286fcb
SergeyG <sergey.galimov.97@mail.ru>
2020-05-17 22:59:14 +0500

    Hello World program

---
+++ Swift1/Swift1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift1
+//
+//  Created by Сергей Галимов on 17.05.2020.
+//  Copyright © 2020 Сергей Галимов. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
